1916cb3e6a838464c40bc7fc4556f3f5e1d602f9
Samantha Iacob <Samantha.Iacob@ons.gov.uk>
2024-04-15 15:14:17 +0100

    First commit

---
+++ files/colors.py
@@ -0,0 +1,12 @@
+'''
+20240515
+samantha iacob, iacobs@ons.gov.uk
+lists of accessible colors taken from https://personal.sron.nl/~pault/
+for use in python plotting and mapping
+to be added to when possible!
+'''
+
+
+pault_vibrant = ['#332288', '#88CCEE', '#44AA99', '#117733', '#999933', '#DDCC77', '#CC6677', '#882255', '#AA4499']
+pault_pale = ['#77AADD', '#99DDFF', '#44BB99', '#BBCC33', '#AAAA00', '#EEDD88', '#EE866', '#FFAABB']
+pault_missing = '#DDDDDD'
